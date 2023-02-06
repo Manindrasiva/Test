f3e68146a2dc8a0c65582007e6e98e121b3b7a58
Maneendar <maneendar0434@gmail.com>
2023-02-06 13:26:35 +0530

    Java_code_06_02_2023

---
+++ Testing/src/OnePointEightFeatures.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class OnePointEightFeatures {
+    public static void main(String[] args) {
+        List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+        long count = strList.stream() .filter(x -> x.isEmpty()) .count();
+        System.out.println(count);
+    }
+}
